6fcb83ad1088b533f4fc62d32ece427f31dbb9bf
Jonay Santana <jonaysantana93@gmail.com>
2020-05-31 19:26:57 +0100

    primer commit

---
+++ public/js/socket.chat.js
@@ -0,0 +1,57 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios')
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+}
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log(resp);
+    });
+
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+/*
+socket.emit('crearMensaje', {
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+});
+*/
+
+//Mensajes privados
+socket.on('mensajePrivado', function(message) {
+    console.log('Mensaje privado ', message);
+})
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+socket.on('listaPersonas', function(personas) {
+
+    console.log(personas);
+
+});
